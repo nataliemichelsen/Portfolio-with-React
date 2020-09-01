bb543c73dbb45fb6e864bb7827244e69612a3b35
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-09-01 15:34:02 -0600

    project.js pages outline-done except for images not ready

--- src/pages/portfolio/project.js
+++ src/pages/portfolio/project.js
@@ -3,10 +3,10 @@ export default function (projectData) {
         case 'password':
             return {
                 title: 'Random Password Generator',
-                img: '',
+                img: './images/password',
                 alt: 'Random Password',
-                gitLink: '',
-                liveLink: '',
+                gitLink: 'https://github.com/nataliemichelsen/PasswordGenerator',
+                liveLink: 'https://nataliemichelsen.github.io/PasswordGenerator/',
                 marginTop: '44px',
                 description: 'Always keep your passwords random.',
             };
@@ -14,10 +14,10 @@ export default function (projectData) {
         case 'quiz':
             return {
                 title: 'Coding Quiz',
-                img: '',
+                img: './images/quiz',
                 alt: 'SMCIV4 Quiz',
-                gitLink: '',
-                liveLink: '',
+                gitLink: 'https://github.com/nataliemichelsen/Coding-Quiz',
+                liveLink: 'https://nataliemichelsen.github.io/Coding-Quiz/',
                 marginTop: '44px',
                 description: 'Do you know your SMCIV4 Quotes? Test your skills with this short quiz.',
             };
@@ -25,10 +25,10 @@ export default function (projectData) {
         case 'scheduler':
             return {
                 title: 'Work Day Scheduler',
-                img: '',
+                img: './images/scheduler',
                 alt: 'Day Planner',
-                gitLink: '',
-                liveLink: '',
+                gitLink: 'https://github.com/nataliemichelsen/Work-Day-Scheduler',
+                liveLink: 'https://nataliemichelsen.github.io/Work-Day-Scheduler/',
                 marginTop: '44px',
                 description: 'Keep track of your schedule, one day at a time.',
             };
@@ -36,10 +36,10 @@ export default function (projectData) {
         case 'weather':
             return {
                 title: 'Weather App',
-                img: '',
+                img: './images/weather',
                 alt: 'Weather API',
-                gitLink: '',
-                liveLink: '',
+                gitLink: 'https://github.com/nataliemichelsen/Weather-API',
+                liveLink: 'https://nataliemichelsen.github.io/Weather-API/',
                 marginTop: '44px',
                 description: 'Check the weather anywhere by ZIP Code.',
             };
@@ -47,10 +47,10 @@ export default function (projectData) {
         case 'profiles':
             return {
                 title: 'Team Profile Generator',
-                img: '',
+                img: './images/profiles',
                 alt: 'Profile Creator',
-                gitLink: '',
-                liveLink: '',
+                gitLink: 'https://github.com/nataliemichelsen/Team-Profile-Generator',
+                liveLink: 'https://nataliemichelsen.github.io/Team-Profile-Generator/',
                 marginTop: '44px',
                 description: 'Create new employee profiles quickly and easily.',
             };
@@ -58,10 +58,10 @@ export default function (projectData) {
         case 'notes':
             return {
                 title: 'Note Taker',
-                img: '',
+                img: './images/notes',
                 alt: 'Notepad',
-                gitLink: '',
-                liveLink: '',
+                gitLink: 'https://github.com/nataliemichelsen/NotesTaker',
+                liveLink: 'https://git.heroku.com/natalie-notes.git',
                 marginTop: '44px',
                 description: 'Keep, save, and delete personal notes with this handy app.',
             };
@@ -69,10 +69,10 @@ export default function (projectData) {
         case 'readme':
             return {
                 title: 'README Generator',
-                img: '',
+                img: './images/readme',
                 alt: 'README Quiz',
-                gitLink: '',
-                liveLink: '',
+                gitLink: 'https://github.com/nataliemichelsen/README-Generator',
+                liveLink: 'https://nataliemichelsen.github.io/README-Generator/',
                 marginTop: '44px',
                 description: 'Create new READMEs with this organized quiz.',
             };
@@ -80,10 +80,10 @@ export default function (projectData) {
         case 'ems':
             return {
                 title: 'Employee Management System',
-                img: '',
+                img: './images/ems',
                 alt: 'Employee Database',
-                gitLink: '',
-                liveLink: '',
+                gitLink: 'https://github.com/nataliemichelsen/Employee-Management-System',
+                liveLink: 'https://nataliemichelsen.github.io/Employee-Management-System/',
                 marginTop: '44px',
                 description: 'Keep track of current employees plus create, delete, or edit new employees.',
             };
@@ -93,8 +93,8 @@ export default function (projectData) {
                 title: 'Node Handlebars',
                 img: '',
                 alt: 'Eat The Burger',
-                gitLink: '',
-                liveLink: '',
+                gitLink: 'https://github.com/nataliemichelsen/Node-Handlebars',
+                liveLink: 'https://git.heroku.com/natalie-node-handlebars.git',
                 marginTop: '44px',
                 description: 'Eat burgers. Keep track of burgers already eaten. Eat more burgers.',
             };
@@ -102,10 +102,10 @@ export default function (projectData) {
         case 'fitness':
             return {
                 title: 'Fitness Tracker',
-                img: '',
+                img: './images/fitness',
                 alt: 'Workout Creator',
-                gitLink: '',
-                liveLink: '',
+                gitLink: 'https://github.com/nataliemichelsen/Fitness-Tracker',
+                liveLink: 'https://git.heroku.com/natalie-fitness.git',
                 marginTop: '44px',
                 description: 'Keep track of past workouts and create new exercises to reach personal fitness goals.',
             };
@@ -113,10 +113,10 @@ export default function (projectData) {
         case 'budget':
             return {
                 title: 'Budget Tracker',
-                img: '',
+                img: './images/budget',
                 alt: 'Budget Tracker',
-                gitLink: '',
-                liveLink: '',
+                gitLink: 'https://github.com/nataliemichelsen/Progressive-Budget',
+                liveLink: 'https://git.heroku.com/natalie-budget.git',
                 marginTop: '44px',
                 description: 'A budget calendar that logs past withdraw and deposit transactions.',
             };
@@ -126,8 +126,8 @@ export default function (projectData) {
                 title: 'User Directory',
                 img: '',
                 alt: 'Random Users',
-                gitLink: '',
-                liveLink: '',
+                gitLink: 'https://github.com/nataliemichelsen/User-Directory',
+                liveLink: 'https://nataliemichelsen.github.io/User-Directory/',
                 marginTop: '44px',
                 description: 'Search the database for users by name.',
             };
@@ -135,10 +135,10 @@ export default function (projectData) {
         case 'stocks':
             return {
                 title: 'Stocks 2.0',
-                img: '',
+                img: './images/stocks',
                 alt: 'Stocks 2.0',
-                gitLink: '',
-                liveLink: '',
+                gitLink: 'https://github.com/nataliemichelsen/Project-1',
+                liveLink: 'https://nataliemichelsen.github.io/Project-1/',
                 marginTop: '44px',
                 description: 'Keep up to date on all stocks. Save your favorites to a quick-list. Check current stock and financial news.',
             };
@@ -146,9 +146,9 @@ export default function (projectData) {
         case 'stories':
             return {
                 title: 'Story Lines',
-                img: '',
+                img: './images/stories',
                 alt: 'Story Lines',
-                gitLink: '',
+                gitLink: 'https://github.com/Jollyrgr83/project-2',
                 liveLink: '',
                 marginTop: '44px',
                 description: 'A simpler task, story, and project management app.',
@@ -159,8 +159,8 @@ export default function (projectData) {
                 title: 'ReciPique',
                 img: '',
                 alt: 'ReciPique',
-                gitLink: '',
-                liveLink: '',
+                gitLink: 'https://github.com/nataliemichelsen/Project-3-Recipe-Index',
+                liveLink: 'https://recipique.herokuapp.com/',
                 marginTop: '44px',
                 description: 'Social Recipe Sharing Platform and Recipe Saver',
             };
