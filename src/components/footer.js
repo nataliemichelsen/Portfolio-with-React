a6a3806146afb28ae6260826176299ea939a7de9
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-09-01 14:13:21 -0600

    same footer.js component from original portfolio

---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+// react import
+import React from 'react';
+import '../components.css';
+
+// class -> className
+function Footer() {
+    return (
+        <div className="footer">
+            <p className="footer-details">© NVM 2020</p>
+            <p className="footer-details">Created using React</p>
+        </div>
+    )
+}
+
+// react export
+export default Footer
