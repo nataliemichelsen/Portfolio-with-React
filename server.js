873cfd56a836adace192be6f5e0fb9a43349ba20
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-09-01 16:34:58 -0600

    server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const server = express();
+
+const PORT = process.env.PORT || 3001;
+
+server.use(express.static(path.join(__dirname, 'client/build')));
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+});
