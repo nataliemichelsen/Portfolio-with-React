1062a94151621b1fa9a71158089592ad99db047b
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-09-01 17:32:04 -0600

    test

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ import App from './App';
 // index css & footer
 import './index.css';
 import Footer from './components/footer/footer';
+import Nav from './components/nav/nav';
 
-ReactDom.render([<App key='1' />, <Footer key='2' />], document.getElementById('root'));
+ReactDom.render([<App key='1' />, <Footer key='2' />, <Nav key='3' />], document.getElementById('root'));
